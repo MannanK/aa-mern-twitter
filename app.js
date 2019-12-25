cc144855e85b0a2a6f13f2ccca6fe6babad77626
Mannan Kasliwal <mkasliw1@binghamton.edu>
2019-12-25 17:21:02 -0500

    Add routes for users and tweets

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
+const port = process.env.PORT || 5000;
 const app = express();
+
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -10,4 +14,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World From Nodemon"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
