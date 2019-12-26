33508c5376673caee0ee842d6e2a310540842820
Mannan Kasliwal <mkasliw1@binghamton.edu>
2019-12-25 19:14:08 -0500

    Add validations for user register and login, move bodyParser config before all route declarations (errors otherwise)

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
 const db = require('./config/keys').mongoURI;
 mongoose
@@ -13,10 +13,10 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World From Nodemon"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
